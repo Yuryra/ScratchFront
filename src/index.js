cb4273b6801bd7e1d780583e26bbec362a87d1fb
Yuryra <y_rapoport@hotmail.com>
2023-02-10 15:40:45 -0500

    added process.env.CI = false;

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "bulma/css/bulma.min.css";
 
-
+process.env.CI = false; // chatGPT says so!\
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
